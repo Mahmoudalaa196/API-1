c3a395f4626130d2342898c8e76e4be590e205f8
Mahmoudalaa196 <mahmoudalaaaldien10@gmail.com>
2025-09-27 02:37:05 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace DAy1.Data
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal salary { get; set; }
+
+    }   
+}
